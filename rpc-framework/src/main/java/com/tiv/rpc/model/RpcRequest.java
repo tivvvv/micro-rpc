8ab12649f2402c317d326398e99552e7c31f0dcb
tivvvv <18612553935@163.com>
2025-02-12 22:31:36 +0800

    feat: Rpc请求和响应.

---
+++ rpc-framework/src/main/java/com/tiv/rpc/model/RpcRequest.java
@@ -0,0 +1,38 @@
+package com.tiv.rpc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Rpc请求
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RpcRequest implements Serializable {
+
+    /**
+     * 服务名
+     */
+    private String serviceName;
+
+    /**
+     * 方法名
+     */
+    private String methodName;
+
+    /**
+     * 参数类型列表
+     */
+    private Class<?>[] parameterTypes;
+
+    /**
+     * 参数列表
+     */
+    private Object[] args;
+}
