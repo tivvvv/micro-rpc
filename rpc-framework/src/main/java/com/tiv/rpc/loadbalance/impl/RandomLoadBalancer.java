bbad7071f2379c84f4f7aa5440792979e703f2af
tivvvv <18612553935@163.com>
2025-03-19 19:28:09 +0800

    feat: 随机负载均衡器.

---
+++ rpc-framework/src/main/java/com/tiv/rpc/loadbalance/impl/RandomLoadBalancer.java
@@ -0,0 +1,29 @@
+package com.tiv.rpc.loadbalance.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import com.tiv.rpc.loadbalance.LoadBalancer;
+import com.tiv.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 随机负载均衡器
+ */
+public class RandomLoadBalancer implements LoadBalancer {
+
+    private final Random random = new Random();
+
+    @Override
+    public ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (CollUtil.isEmpty(serviceMetaInfoList)) {
+            return null;
+        }
+        int size = serviceMetaInfoList.size();
+        if (size == 1) {
+            return serviceMetaInfoList.get(0);
+        }
+        return serviceMetaInfoList.get(random.nextInt(size));
+    }
+}
