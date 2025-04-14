5652bfe5577f5117831470d1fa69dcc884aa6ad1
tivvvv <18612553935@163.com>
2025-04-15 07:58:05 +0800

    feat: rpc框架自启动器.

---
+++ rpc-spring-boot-starter/src/main/java/com/tiv/rpc/springboot/starter/bootstrap/RpcInitBootstrap.java
@@ -0,0 +1,31 @@
+package com.tiv.rpc.springboot.starter.bootstrap;
+
+import com.tiv.rpc.config.RpcConfig;
+import com.tiv.rpc.config.RpcConfigHolder;
+import com.tiv.rpc.server.tcp.VertxTcpServer;
+import com.tiv.rpc.springboot.starter.annotation.EnableRpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * rpc框架自启动器
+ */
+@Slf4j
+public class RpcInitBootstrap implements ImportBeanDefinitionRegistrar {
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+
+        // 获取@EnableRpc的needServer属性值
+        boolean needServer = (boolean) importingClassMetadata.getAnnotationAttributes(EnableRpc.class.getName()).get("needServer");
+        // 初始化配置
+        RpcConfigHolder.init();
+        RpcConfig rpcConfig = RpcConfigHolder.getRpcConfig();
+
+        if (needServer) {
+            log.info("micro-rpc server启动.");
+            new VertxTcpServer().doStart(rpcConfig.getPort());
+        }
+    }
+}
